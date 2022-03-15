336169d2c0998ab9e60f6cfc808dabae0389b1ee
Rahul <koderrahul@gmail.com>
2022-03-15 14:41:35 +0530

    Co-authored-by: Vanshika Rana <Vanshika-Rana@users.noreply.github.com>

---
+++ src/Components/Navbar.js
@@ -0,0 +1,31 @@
+import "./Navbar.css"
+
+import { FaBars} from "react-icons/fa";
+
+function Navbar(){
+
+    return ( 
+
+        <nav className="navbar">
+        <header className="navigation">
+            <h3>logo</h3>
+            <ul className="nav-links">
+              <li><a href="/#">HOME</a></li>
+              <li><a href="/#">VIDEO</a></li>
+              <li><a href="/#">POST</a></li>
+              <li><a href="/#">BROWSE</a></li>
+
+            <button>
+            <FaBars/>
+            </button>
+
+            </ul>
+
+        </header>
+        </nav>
+
+     );
+
+}
+
+export default Navbar;
